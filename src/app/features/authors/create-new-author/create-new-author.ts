74caa4e3389081d26c7acb3ee2f436291ad52842
Elena Rottin <elena.rottin@gmail.com>
2025-09-29 22:23:46 +0200

    create author component (empty)

---
+++ src/app/features/authors/create-new-author/create-new-author.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-new-author',
+  imports: [],
+  templateUrl: './create-new-author.html',
+  styleUrl: './create-new-author.scss'
+})
+export class CreateNewAuthor {
+
+}
